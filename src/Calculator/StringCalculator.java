3ce407fa33b1f0ec7af5a9e4eb579b23e2723d19
AbhilashaSingla <abhilashasingla.as@gmail.com>
2020-07-02 00:56:03 +0530

    Second Commit

--- src/Calculator/StringCalculator.java
+++ src/Calculator/StringCalculator.java
@@ -1,8 +1,73 @@
 package Calculator;
-public class StringCalculator {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class StringCalculator {
+	static int GETCOUNTER;
+	
+	public static int Add(String numbers) {
+		GETCOUNTER++;
+		if(numbers.isEmpty()) {
+		return 0;
+		}
+		else {
+			String[] addNum;
+			if(numbers.startsWith("//")) {
+				addNum=splitUsingCustomDelimeter(numbers);
+			}
+			else{
+				addNum= numbers.split(",|\n");
+			}
+			int i,sum=0;
+			List<String> arrNumNeg= new ArrayList<>();
+			for(i=0;i<addNum.length;i++) {
+				int num=Integer.parseInt(addNum[i]);
+				if(num<0) {
+					arrNumNeg.add(addNum[i]);
+				}
+				else if(num>1000){
+					continue;
+					
+				}
+				else {
+					sum=sum+num;
+				}
+			}
+			if(arrNumNeg.size()>0) {
+				throw new RuntimeException("negatives not allowed: " + arrNumNeg);
+			}
+			return sum;
+		}
+		
+
+	}
+	
+	private static String[] splitUsingCustomDelimeter(String numbers) {
+		Matcher del = Pattern.compile("-?\\d+").matcher(numbers);
+		int a,j=0;
+		List<Integer> addNum = new ArrayList<Integer>();
+		for(a=0;a<numbers.length()-1;a++) {
+			if(del.find()) {
+				 int num= Integer.parseInt(del.group());
+				 addNum.add(num);
+			}
+		}
+		String str = "" ;
+		 for(j=0;j<addNum.size();j++) {
+				int num1=addNum.get(j);
+				str=str+num1;
+				}
+		String strArray[] = str.split("");
+		return strArray;
+	}
+	
+	public static int GetCalledCount(){
+		return GETCOUNTER;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/Calculator/StringCalculatorTest.java
+++ src/Calculator/StringCalculatorTest.java
@@ -7,8 +7,96 @@ import org.junit.jupiter.api.Test;
 class StringCalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void returnZeroOnEmpty() {
+		assertEquals(0, StringCalculator.Add(""));
+	}
+	
+	@Test
+	void returnOnOneNumber() {
+		assertEquals(10, StringCalculator.Add("10"));
+	}
+	
+	@Test
+	void returnOnTwoNumber() {
+		assertEquals(20, StringCalculator.Add("10,10"));
+	}
+	
+	@Test
+	void returnOnUnlimitedNumbers() {
+		assertEquals(6, StringCalculator.Add("1,2,3"));
+	}
+	
+	@Test
+	void returnOnNewLine() {
+		assertEquals(6, StringCalculator.Add("1,2\n3"));
+	}
+	
+	@Test
+	void returnOnCustomDelimeter() {
+		assertEquals(3, StringCalculator.Add("//;\n1;2"));
+	}
+	
+	@Test
+	void returnOnCustomDelimeter1() {
+		assertEquals(3, StringCalculator.Add("//*\n1*2"));
+	}
+	
+	@Test
+	void negativeNumber() {
+		try{
+			StringCalculator.Add("-1,2,3");
+			fail("“negatives not allowed" );
+		}
+		catch(RuntimeException ex){
+		}
+	}
+	
+	@Test
+	void negativeNumberPresent() {
+		try{
+			StringCalculator.Add("-1,2,3");
+			fail("“negatives not allowed" );
+		}
+		catch(RuntimeException ex){
+			assertEquals("negatives not allowed: [-1]",ex.getMessage() );
+		}
+	}
+	
+	@Test
+	void negativeNumberPresent1() {
+		try{
+			StringCalculator.Add("-1,-2,3");
+			fail("“negatives not allowed" );
+		}
+		catch(RuntimeException ex){
+			assertEquals("negatives not allowed: [-1, -2]",ex.getMessage() );
+		}
+	}
+	
+	@Test
+	void returnOnGetCalledCount() {
+		int getCalledCount=StringCalculator.GetCalledCount();
+		assertEquals(getCalledCount, StringCalculator.GetCalledCount());
+	}
+	
+	@Test
+	void returnOnLargeNumber() {
+		assertEquals(2, StringCalculator.Add("1001,2"));
+	}
+	
+	@Test
+	void returnOnDelimeterAnyLength() {
+		assertEquals(6, StringCalculator.Add("//[***]\\n1***2***3"));
+	}
+	
+	@Test
+	void returnOnMultipleDelimeter() {
+		assertEquals(6, StringCalculator.Add("//[*][%]\\n1*2%3"));
+	}
+	
+	@Test
+	void returnMultipleDelimterAnyLength() {
+		assertEquals(6, StringCalculator.Add("//[**][%%]\\n1**2%%3"));
 	}
 
 }
